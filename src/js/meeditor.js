40647d899bb41afe56bda41bb3c2a1b9f6ab539d
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-05 22:39:35 -0300

    Função create editor

--- src/js/meeditor.js
+++ src/js/meeditor.js
@@ -1,7 +1,42 @@
 const MEEditor = {
   selector: null,
+  target: {
+    content: null,
+    buttons: null
+  },
+  createEditor: ({ selector, target }) => {
+    const editor = document.createElement('div')
+    const editorToolbar = document.createElement('div')
+    const editorToolbarButtons = document.createElement('div')
+    const editorWrapper = document.createElement('div')
+    const editorWrapperContent = document.createElement('div')
+
+    editor.className = 'mee-box'
+    editorToolbar.className = 'mee-toolbar'
+    editorToolbarButtons.className = 'mee-buttons'
+    editorWrapper.className = 'mee-wrapper'
+    editorWrapperContent.className = 'mee-content'
+
+    editorWrapperContent.setAttribute('contenteditable', true)
+
+    editorToolbar.appendChild(editorToolbarButtons)
+    editorWrapper.appendChild(editorWrapperContent)
+
+    editor.appendChild(editorToolbar)
+    editor.appendChild(editorWrapper)
+
+    selector.replaceWith(editor)
+
+    target.buttons = editorToolbarButtons
+    target.content = editorWrapperContent
+
+    return
+  },
   start: function({ selector }) {
     this.selector = document.querySelector(selector)
+    this.createEditor(this)
+
+    return
   }
 }
 
